83947d75ca1909a6487314592ca1ebcc60162fa2
Oswaldo Rodriguez <Owner2303@gmail.com>
2021-12-07 12:03:56 -0500

    this

---
+++ scripts.js
@@ -0,0 +1,13 @@
+var date = new Date();
+var n = date.getFullYear();
+console.log(n); 
+var year = document.getElementById('year').innerHTML = n;
+
+let date = new Date();
+
+const speechBox ={
+    date = 0,
+    link = '',
+    speaker = ''
+}
+console.log(speechBox);
