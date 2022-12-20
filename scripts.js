cb6a283c7f031ee3fe6e4c01ec4533f3948c240b
Oswaldo Rodriguez <Owner2303@gmail.com>
2022-12-20 15:26:23 -0500

    adding the new talks for 2022

--- scripts.js
+++ scripts.js
@@ -10,90 +10,68 @@ var year = document.getElementById('year').innerHTML = n;
 const speechBox = [
 
     {
-        my_date: new Date(2022, 4, 8),
+        my_date: new Date(2022, 11, 25),
         link: "https://www.churchofjesuschrist.org/study/general-conference/2022/04/14bednar?lang=spa",
-        title: "“Pero no les hicimos caso”",
-        img :'https://assets.ldscdn.org/3ae9962a4b9604a4cc5d20360f5f15eda99f1bd6',
-        speaker: "élder David A. Bednar"
+        title: "Navidad",
+        img :'https://www.churchofjesuschrist.org/bc/content/ldsorg/home-strata-live/2022/743_Seeking-Christ-in-our-Christmas-Traditions_L2_I-10_1f83673e13f9f5631806404a520d92858d48c2b1_Church%20of%20Jesus%20Christ.jpg',
+        speaker: ""
     },
     {
-        my_date: new Date(2022, 4, 22),
+        my_date: new Date(2023, 0, 8),
+        link: "https://www.churchofjesuschrist.org/study/general-conference/2022/10/53schmitt?lang=spa",
+        title: "“Que te conozcan a Ti”",
+        img :'https://assets.churchofjesuschrist.org/9acc4a289d6393b19130a290a75a10f72cc9664a.jpeg ',
+        speaker: "élder Jonathan S. Schmitt"
+    },
+    {
+        my_date: new Date(2023, 0, 22),
         link: "https://www.churchofjesuschrist.org/study/general-conference/2022/04/15andersen?lang=spa",
-        title: "Seguir a Jesús: ser pacificadores",
-        img:"https://assets.ldscdn.org/8a1f9b8dc23a580b2e197a9a3a4dc70926ef9288",
-        speaker: "élder Neil L. Andersen"
+        title: "Un discipulado que perdura",
+        img:"https://assets.churchofjesuschrist.org/faae4cb7ebbe4891bbf13b6e5787210739dcb3f1.jpeg",
+        speaker: "presidente Steven J. Lund"
     },
     {
-        my_date: new Date(2022, 5, 26),
-        link: "https://www.churchofjesuschrist.org/study/general-conference/2022/04/18eyring?lang=spa",
-        img:'https://assets.ldscdn.org/bd/0a/bd0ad5c2eac5ec6fe2bf1b5b5769f29be8d6ce5b/bd0ad5c2eac5ec6fe2bf1b5b5769f29be8d6ce5b.image/jpeg',
-        title: "Firmes en las tormentas",
-        speaker: "presidente Henry B. Eyring"
+        my_date: new Date(2023, 1, 12),
+        link: "https://www.churchofjesuschrist.org/study/general-conference/2022/10/44sitati?lang=spa",
+        img:'https://assets.churchofjesuschrist.org/67a06655887b85f476bb78209fc4e1d59c2e25b2.jpeg',
+        title: "Modelos de discipulado",
+        speaker: " élder Joseph W. Sitati"
     },
     {
-        my_date: new Date(2022, 6, 10),
+        my_date: new Date(2023, 1, 26),
         link: "https://www.churchofjesuschrist.org/study/general-conference/2022/04/23holland?lang=spa",
-        img: 'https://assets.ldscdn.org/6e9a94308635adadc3dc77791bd19fac7e8dd3f4',
-        title: "No temas; cree solamente",
-        speaker: "élder Jeffrey R. Holland"
-    },
-    {
-        my_date: new Date(2022, 6, 24),
-        link: "https://www.churchofjesuschrist.org/study/general-conference/2022/04/26gong?lang=spa",
-        img: 'https://assets.ldscdn.org/220cc00a28d550c263c62bb624424a5c0d365009',
-        title: "Cada uno de nosotros tiene una historia",
+        img: 'https://assets.churchofjesuschrist.org/ccf3f45774ffe1449f48ea50d3211c268a9feb11.jpeg',
+        title: "Felices y para siempre",
         speaker: "élder Gerrit W. Gong"
     },
     {
-        my_date: new Date(2022, 7, 14),
-        link: "https://www.churchofjesuschrist.org/study/general-conference/2022/04/29cook?lang=spa",
-        img: 'https://assets.ldscdn.org/be193a530a78d675e0fb3c8540a05a3790c93ce8',
-        title: "Conversión a la voluntad de Dios",
-        speaker: "élder Quentin L. Cook"
+        my_date: new Date(2023, 2, 12),
+        link: "https://www.churchofjesuschrist.org/study/general-conference/2022/10/34silva?lang=spa",
+        img: 'https://assets.churchofjesuschrist.org/849860dd6985918a077b8309c15b0d378992df99.jpeg',
+        title: "Valentía para proclamar la verdad",
+        speaker: "élder Denelson Silva"
     },
     {
-        my_date: new Date(2022, 7, 28),
-        link: "https://www.churchofjesuschrist.org/study/general-conference/2022/04/36renlund?lang=spa",
-        img: 'https://assets.ldscdn.org/afdbb7e8dc2deee83f180d5ccac5e38f8115e4f7',
-        title: "Su naturaleza divina y destino eterno",
-        speaker: "élder Dale G. Renlund"
+        my_date: new Date(2023, 2, 26),
+        link: "https://www.churchofjesuschrist.org/study/general-conference/2022/10/33pearson?lang=spa",
+        img: 'https://assets.churchofjesuschrist.org/dda08902f6a20109d15dadc7eec1519e93733fe0.jpeg',
+        title: "¿Aún están dispuestos?",
+        speaker: "élder Kevin W. Pearson"
     },
     {
-        my_date: new Date(2022, 8, 11),
-        link: "https://www.churchofjesuschrist.org/study/general-conference/2022/04/41christofferson?lang=spa",
-        title: "Nuestra relación con Dios",
-        img:'https://assets.ldscdn.org/fe/8a/fe8aa750460018cb27bd86c2b55001f88cc53ff9/fe8aa750460018cb27bd86c2b55001f88cc53ff9.image/jpeg',
-        speaker: "élder D. Todd Christofferson"
-    },
-    {
-        my_date: new Date(2022, 8, 25),
-        link: "https://www.churchofjesuschrist.org/study/general-conference/2022/04/45rasband?lang=spa",
-        img:'https://assets.ldscdn.org/5d/c4/5dc46e8dad285884173744f6b723eaf6d25a1039/5dc46e8dad285884173744f6b723eaf6d25a1039.image/jpeg',
-        title: "Sanar al mundo",
-        speaker: "élder Ronald A. Rasband"
-    },
-    {
-        my_date: new Date(2022, 9, 2),
+        my_date: new Date(2023, 3, 2),
         link: "https://www.churchofjesuschrist.org/?lang=spa",
         img: 'https://assets.ldscdn.org/fd7cc4be8be035ae5801f4904dad9d48c72b2b5e',
         title: "Conferencia General",
         speaker: "Autoridades Generales"
     },
     {
-        my_date: new Date(2022, 9, 9),
-        link: "https://www.churchofjesuschrist.org/study/general-conference/2022/04/47nelson?lang=spa",
-        img: 'https://assets.ldscdn.org/e28bb7f79a5d19151565a3563a85bd5533682eea',
-        title: "El poder del ímpetu espiritual",
-        speaker: "presidente Russell M. Nelson"
+        my_date: new Date(2023, 3, 9),
+        link: "https://www.churchofjesuschrist.org/study/general-conference/2022/10/27zeballos?lang=spa",
+        title: "Edificar una vida resistente al adversario",
+        img:'https://assets.churchofjesuschrist.org/acfc8a5c166f64f27fb0330a943b62f2710ced90.jpeg',
+        speaker: "élder Jorge F. Zeballos"
     },
-    {
-        my_date: new Date(2022, 9, 23),
-        link: "https://www.churchofjesuschrist.org/study/general-conference/2022/04/51oaks?lang=spa",
-        img: 'https://assets.ldscdn.org/7818d5910f20601014039f0d4332cc280b0358ce',
-        title: "El amor divino en el plan del Padre",
-        speaker: "Presidente Dallin H. Oaks"
-    },
-
 
     // { my_date: 'Apr 3', link: "https://www.churchofjesuschrist.org/?lang=spa", title: "Conferencia General", speaker: "Autoridades Generales" },
 ]
