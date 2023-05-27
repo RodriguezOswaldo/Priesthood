310298484b227dd9de492e56805f3a8742a66624
Oswaldo Rodriguez <Owner2303@gmail.com>
2023-05-27 15:09:55 -0400

    Adding talks may-october

--- scripts.js
+++ scripts.js
@@ -10,67 +10,109 @@ var year = document.getElementById('year').innerHTML = n;
 const speechBox = [
 
     {
-        my_date: new Date(2022, 11, 25),
+        my_date: new Date(2023, 11, 25),
         link: "https://www.churchofjesuschrist.org/study/general-conference/2022/04/14bednar?lang=spa",
         title: "Navidad",
-        img :'https://www.churchofjesuschrist.org/bc/content/ldsorg/home-strata-live/2022/743_Seeking-Christ-in-our-Christmas-Traditions_L2_I-10_1f83673e13f9f5631806404a520d92858d48c2b1_Church%20of%20Jesus%20Christ.jpg',
+        img: 'https://www.churchofjesuschrist.org/bc/content/ldsorg/home-strata-live/2022/743_Seeking-Christ-in-our-Christmas-Traditions_L2_I-10_1f83673e13f9f5631806404a520d92858d48c2b1_Church%20of%20Jesus%20Christ.jpg',
         speaker: ""
     },
     {
-        my_date: new Date(2023, 0, 8),
+        my_date: new Date(2023, 5, 23),
         link: "https://www.churchofjesuschrist.org/study/general-conference/2022/10/53schmitt?lang=spa",
         title: "“Que te conozcan a Ti”",
-        img :'https://assets.churchofjesuschrist.org/9acc4a289d6393b19130a290a75a10f72cc9664a.jpeg ',
+        img: 'https://assets.churchofjesuschrist.org/9acc4a289d6393b19130a290a75a10f72cc9664a.jpeg ',
         speaker: "élder Jonathan S. Schmitt"
     },
     {
-        my_date: new Date(2023, 0, 22),
+        my_date: new Date(2023, 6, 14),
         link: "https://www.churchofjesuschrist.org/study/general-conference/2022/04/15andersen?lang=spa",
         title: "Un discipulado que perdura",
-        img:"https://assets.churchofjesuschrist.org/faae4cb7ebbe4891bbf13b6e5787210739dcb3f1.jpeg",
+        img: "https://assets.churchofjesuschrist.org/faae4cb7ebbe4891bbf13b6e5787210739dcb3f1.jpeg",
         speaker: "presidente Steven J. Lund"
     },
     {
-        my_date: new Date(2023, 1, 12),
+        my_date: new Date(2023, 6, 28),
         link: "https://www.churchofjesuschrist.org/study/general-conference/2022/10/44sitati?lang=spa",
-        img:'https://assets.churchofjesuschrist.org/67a06655887b85f476bb78209fc4e1d59c2e25b2.jpeg',
+        img: 'https://assets.churchofjesuschrist.org/67a06655887b85f476bb78209fc4e1d59c2e25b2.jpeg',
         title: "Modelos de discipulado",
         speaker: " élder Joseph W. Sitati"
     },
     {
-        my_date: new Date(2023, 1, 26),
+        my_date: new Date(2023, 7, 11),
         link: "https://www.churchofjesuschrist.org/study/general-conference/2022/04/23holland?lang=spa",
         img: 'https://assets.churchofjesuschrist.org/ccf3f45774ffe1449f48ea50d3211c268a9feb11.jpeg',
         title: "Felices y para siempre",
         speaker: "élder Gerrit W. Gong"
     },
     {
-        my_date: new Date(2023, 2, 12),
+        my_date: new Date(2023, 7, 25),
         link: "https://www.churchofjesuschrist.org/study/general-conference/2022/10/34silva?lang=spa",
         img: 'https://assets.churchofjesuschrist.org/849860dd6985918a077b8309c15b0d378992df99.jpeg',
         title: "Valentía para proclamar la verdad",
         speaker: "élder Denelson Silva"
     },
     {
-        my_date: new Date(2023, 2, 26),
+        my_date: new Date(2023, 8, 9),
         link: "https://www.churchofjesuschrist.org/study/general-conference/2022/10/33pearson?lang=spa",
         img: 'https://assets.churchofjesuschrist.org/dda08902f6a20109d15dadc7eec1519e93733fe0.jpeg',
         title: "¿Aún están dispuestos?",
         speaker: "élder Kevin W. Pearson"
     },
     {
-        my_date: new Date(2023, 3, 2),
+        my_date: new Date(2023, 8, 23),
+        link: "https://www.churchofjesuschrist.org/study/general-conference/2023/04/17eyring?lang=eng",
+        img: 'https://assets.churchofjesuschrist.org/eb6e865a60734450cc769178ab3da519846cd8e8.jpeg',
+        title: "Encontrando Paz Personal",
+        speaker: "President Henry B. Eyring"
+    },
+    {
+        my_date: new Date(2023, 9, 5),
         link: "https://www.churchofjesuschrist.org/?lang=spa",
         img: 'https://assets.ldscdn.org/fd7cc4be8be035ae5801f4904dad9d48c72b2b5e',
         title: "Conferencia General",
         speaker: "Autoridades Generales"
     },
     {
-        my_date: new Date(2023, 3, 9),
+        my_date: new Date(2023, 9, 13),
         link: "https://www.churchofjesuschrist.org/study/general-conference/2022/10/27zeballos?lang=spa",
         title: "Edificar una vida resistente al adversario",
-        img:'https://assets.churchofjesuschrist.org/acfc8a5c166f64f27fb0330a943b62f2710ced90.jpeg',
+        img: 'https://assets.churchofjesuschrist.org/acfc8a5c166f64f27fb0330a943b62f2710ced90.jpeg',
         speaker: "élder Jorge F. Zeballos"
+    },
+    {
+        my_date: new Date(2023, 9, 27),
+        link: "https://www.churchofjesuschrist.org/study/general-conference/2022/10/27zeballos?lang=spa",
+        title: "Edificar una vida resistente al adversario",
+        img: 'https://assets.churchofjesuschrist.org/acfc8a5c166f64f27fb0330a943b62f2710ced90.jpeg',
+        speaker: "élder Jorge F. Zeballos"
+    },
+    {
+        my_date: new Date(2023, 10, 10),
+        link: "https://www.churchofjesuschrist.org/study/general-conference/2022/10/27zeballos?lang=spa",
+        title: "Edificar una vida resistente al adversario",
+        img: 'https://assets.churchofjesuschrist.org/acfc8a5c166f64f27fb0330a943b62f2710ced90.jpeg',
+        speaker: "élder Jorge F. Zeballos"
+    },
+    {
+        my_date: new Date(2023, 10, 24),
+        link: "https://www.churchofjesuschrist.org/study/general-conference/2022/10/27zeballos?lang=spa",
+        title: "Edificar una vida resistente al adversario",
+        img: 'https://assets.churchofjesuschrist.org/acfc8a5c166f64f27fb0330a943b62f2710ced90.jpeg',
+        speaker: "élder Jorge F. Zeballos"
+    },
+    {
+        my_date: new Date(2023, 11, 8),
+        link: "https://www.churchofjesuschrist.org/study/general-conference/2022/10/27zeballos?lang=spa",
+        title: "Edificar una vida resistente al adversario",
+        img: 'https://assets.churchofjesuschrist.org/acfc8a5c166f64f27fb0330a943b62f2710ced90.jpeg',
+        speaker: "élder Jorge F. Zeballos"
+    },
+    {
+        my_date: new Date(2023, 11, 22),
+        link: "https://www.churchofjesuschrist.org/study/general-conference/2023/04/15cook?lang=eng",
+        title: "Edificar una vida resistente al adversario",
+        img: 'https://assets.churchofjesuschrist.org/25eb9fdb0382ec1d3b8eb6bf05dae99f61e4a89e.jpeg',
+        speaker: "élder Quentin L. Cook"
     },
 
     // { my_date: 'Apr 3', link: "https://www.churchofjesuschrist.org/?lang=spa", title: "Conferencia General", speaker: "Autoridades Generales" },
